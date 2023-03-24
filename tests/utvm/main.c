4bf5b29b5f296d666798cf156859cea085c7028f
Koen Zandberg <koen@bergzand.net>
2023-03-24 13:45:21 +0100

    tests/utvm: add minimal test application

---
+++ tests/utvm/main.c
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2023 Koen Zandberg <koen@bergzand.net>
+ * Copyright (C) 2023 Zhaolan Huang <zhaolan.huang@fu-berlin.de>
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     tests
+ * @{
+ *
+ * @file
+ * @brief       Test application for uTVM models
+ *
+ * @author      Kaspar Schleiser <kaspar@schleiser.de>
+ * @author      Oliver Hahm <oliver.hahm@inria.fr>
+ * @author      Ludwig Knüpfer <ludwig.knuepfer@fu-berlin.de>
+ *
+ * @}
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+
+int main(void)
+{
+    (void) puts("uTVM test application");
+
+    return 0;
+}
