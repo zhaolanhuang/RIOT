c5b6184b8779e9ee3637b87d656000f928a64597
Koen Zandberg <koen@bergzand.net>
2023-03-27 09:52:51 +0200

    utvm: More makefile fiddling

--- makefiles/utvm.inc.mk
+++ makefiles/utvm.inc.mk
@@ -2,11 +2,12 @@
 UTVM_DIR_BASE ?= $(BUILD_DIR)/utvm
 UTVM_NAME = $(UTVM_TAR:%.tar=%)
 UTVM_MODEL_DIR = $(UTVM_DIR_BASE)/$(UTVM_NAME)
-UTVM_METADATA_JSON := $(UTVM_TAR:%.tar=$(UTVM_DIR_BASE)/%/metadata.json)
+UTVM_METADATA_JSON = $(UTVM_TAR:%.tar=$(UTVM_DIR_BASE)/%/metadata.json)
 UTVM_BIN = $(UTVM_NAME:%=$(BINDIR)/%/%.o)
+UTVM_INCLUDES = $(UTVM_NAME:%=-I$(UTVM_DIR_BASE)/%/runtime/include)
+UTVM_INCLUDES += $(UTVM_NAME:%=-I$(UTVM_DIR_BASE)/%/codegen/host/include)
 
-CFLAGS += -I$(UTVM_MODEL_DIR)/runtime/include
-CFLAGS += -I$(UTVM_MODEL_DIR)/codegen/host/include
+CFLAGS += $(UTVM_INCLUDES)
 
 .PRECIOUS: $(UTVM_MODEL_DIR)/.
 
@@ -16,9 +17,10 @@ $(UTVM_MODEL_DIR)/.:
 $(UTVM_METADATA_JSON): $(UTVM_TAR) $(UTVM_MODEl_DIR) | $(UTVM_TAR) $$(@D)/.
 	tar --extract --file=$< --directory $(UTVM_MODEL_DIR) --touch
 
+export $(UTVM_MODEL_DIR)
 $(UTVM_BIN): $(UTVM_METADATA_JSON)
 	$(QQ)"$(MAKE)" -C $(UTVM_MODEL_DIR)/codegen/host/src -f $(RIOTBASE)/makefiles/utvm/Makefile.utvm UTVM_MODULE_NAME=$(UTVM_NAME) UTVM_MODEL_DIR=$(UTVM_MODEL_DIR)
 
+$(BASELIBS): | $(UTVM_BIN)
 $(OBJC) $(OBJCXX): $(UTVM_BIN)
-$(BASELIBS): | $(UTVM_METADATA_JSON)
-all: $(UTVM_BIN)
+link: | $(UTVM_BIN)
